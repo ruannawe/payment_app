853601d9fc1423a7880b7ae18fb3c4bebfdf3681
Ruan Nawe <ruannawe95@outlook.com>
2023-12-05 19:14:08 -0300

    adjust css of PaymentAction edit page

--- payment_app/settings.py
+++ payment_app/settings.py
@@ -116,7 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Using pathlib
+    # os.path.join(BASE_DIR, 'static'),  # Using os.path for older Django versions or if not using pathlib
+]
+
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
