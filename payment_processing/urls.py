c13a81c43876a0bc182605be29f7295b113323f1
Ruan Nawe <ruannawe95@outlook.com>
2023-12-05 01:31:10 -0300

    create edit/update form and django views

--- payment_processing/urls.py
+++ payment_processing/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index  # Import your views
+from .views import index, edit, update  # Import your views
 
 urlpatterns = [
     path('', index, name='index'),  # Define your URL patterns
+    path('edit/<int:id>/', edit, name='edit'),
+    path('update/<int:id>/', update, name='update'),
 ]
--- payment_processing/views.py
+++ payment_processing/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
-from payment_processing.models import PaymentType
+from django.shortcuts import render, get_object_or_404, redirect
+from payment_processing.models import PaymentType, PaymentAction
 
 
 def index(request):
     payment_types = PaymentType.objects.all()
     return render(request, 'index.html', {'payment_types': payment_types})
+
+
+def edit(request, id):
+    payment_type = get_object_or_404(PaymentType, id=id)
+    payment_actions = payment_type.actions.all()
+    all_actions = PaymentAction.objects.exclude(id__in=payment_actions.values_list('id', flat=True))
+
+    return render(request, 'edit.html', {
+        'payment_type': payment_type,
+        'payment_actions': payment_actions,
+        'all_actions': all_actions
+    })
+
+
+def update(request, id):
+    payment_type = get_object_or_404(PaymentType, id=id)
+
+    if request.method == 'POST':
+        print(request.POST)
+        if 'remove_action' in request.POST:
+            action_id = request.POST.get('remove_action')
+            payment_type.actions.remove(action_id)
+        elif 'add_action' in request.POST:
+            action_id = request.POST.get('new_action')
+            payment_type.actions.add(action_id)
+
+        return redirect('index')
+
+    return redirect('edit', id=id)
